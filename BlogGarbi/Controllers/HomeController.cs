7b6c1192aaf71996abd09647650c046822fe4cbc
lukus-git <lucas.garbi13@hotmail.com>
2025-08-08 20:34:50 -0300

    criando camada de dados

--- BlogGarbi/Controllers/HomeController.cs
+++ BlogGarbi/Controllers/HomeController.cs
@@ -15,6 +15,30 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewData["Mensagem"] = "ola mundo";
+        //Criar objetos
+        Categoria categoria = new();
+        categoria.Id = 1;
+        categoria.Nome = "Tecnologia";
+
+        Categoria categoria2 = new()
+        {
+            Id = 2,
+            Nome = "IA"
+        };
+
+        List<Postagem> postagens = [
+            new(){
+                Id = 1,
+                Nome = "Saiu o ChatGPT 5!!!",
+                CategoriaId = 2,
+                Categoria = categoria2,
+                DataPostagem = DateTime.Parse("07/08/2025"),
+                Descricao = "estou com preguiça de pensar nisso",
+                Texto = "Lorem"
+            }
+        ];
+
         return View();
     }
 
